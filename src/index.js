864e9eded360fa5a6b91296a49d81c98ba4f476a
lexBogos <lex.bogos@gmail.com>
2018-09-25 12:03:54 +0300

    Solution_zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    let count = 0;
+    const a = Math.floor(number/5);
+    let b = 0;
+    while(number/Math.pow(5,b)>5) {b++}
+            
+    function schetchik(a,b){
+    	if(b==0)
+    		{return 0}
+    	else
+    		{return a + schetchik(Math.floor(a/5), b-1)}
+        }	
+    count = schetchik(a,b)
+    return count
+    
+    }
